2494bf62e1ccca075198fbfd8e6990e661a59079
ghadeerMayya <ghadeer.mayya.mecha@gmail.com>
2020-07-24 21:32:37 +0200

    Basheer i added all

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,6 @@
 use App\Penifit;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', 'HomeController@index');
 // Route::get()
 
@@ -59,8 +48,6 @@ Route::prefix('reception')->group(function () {
     Route::get('getwaitingusers','Reception\ReceptionController@getwaitingusers')->name('getwaitingusers');
     Route::get('getpenifitlog','Reception\ReceptionController@getPenifitLog')->name('getpenifitlog');
     Route::post('searchid','Reception\ReceptionController@searchCardID')->name('searchid');
-
-
 
     ###### test route ######
 
